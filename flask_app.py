d830108efcebfe039ec3012206aecc75445cd748
Joshua Fernandes <josh.fernandes42@gmail.com>
2023-06-12 14:25:38 -0400

    move counter

--- flask_app.py
+++ flask_app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect, url_for,request,session
 from flask_session import Session
-
+import sqlite3
 import json
 import chess
 from analysis import *
@@ -11,6 +11,11 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+connect = sqlite3.connect('database.db')
+connect.execute(
+    'CREATE TABLE IF NOT EXISTS PARTICIPANTS (name TEXT, \
+    games INTEGER)')
+
 @app.route('/tutor')
 def index():
     return render_template("index.html")
@@ -19,16 +24,20 @@ def index():
 def hello_world():
     if request.method == "POST":
         session['count'] = 0
+        session['move'] = 0
+
         return render_template("index.html")
 
     return render_template("consent.html")
 
 @app.route('/move/<int:depth>/<path:fen>/')
 def get_move(depth, fen):
-    if session.get('count') >= depth:
-        check = False
-    else:
+    print(session['count'],session['move'])
+    session['move'] =  session.get('move') + 1
+    if session.get('count') <= depth and session.get('move') > 5:
         check = True
+    else:
+        check = False
     print(session.get('count'),check)
     sf_move, leela_next_move_for_player = sf_calc(fen, check)
     if leela_next_move_for_player:
