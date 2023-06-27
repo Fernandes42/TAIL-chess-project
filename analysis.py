81ecfbdab27b45fd78ac7d95e9e5d72fcf6ff7fe
Joshua Fernandes <josh.fernandes42@gmail.com>
2023-06-27 14:49:49 -0400

    linux changes

--- analysis.py
+++ analysis.py
@@ -2,9 +2,10 @@ import chess
 import chess.engine
 
 def sf_calc(fen, check):
-    stockfish = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/stockfish/15.1/bin/stockfish")
-    leela = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/lc0/0.29.0/bin/lc0")
-
+    # stockfish = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/stockfish/15.1/bin/stockfish")
+    # leela = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/lc0/0.29.0/bin/lc0")
+    stockfish = chess.engine.SimpleEngine.popen_uci("/home/jfernandes33/packages/stockfish/15.1/bin/stockfish")
+    leela = chess.engine.SimpleEngine.popen_uci("/home/jfernandes33/packages/lc0/0.29.0/bin/lc0")
     board = chess.Board()
     board.set_fen(fen)
     response = stockfish.analyse(board, chess.engine.Limit(time=.1))
@@ -22,7 +23,8 @@ def sf_calc(fen, check):
     else:
         next_move = None
 
+    print("Score:", sf['score'])
+
     stockfish.quit()
     leela.quit()
-
     return chess.Move.uci(response['pv'][0]), next_move
