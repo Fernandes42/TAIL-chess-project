34a6872eeb86f73e6e28b8fd52f662d4c59f5395
Joshua Fernandes <josh.fernandes42@gmail.com>
2023-06-29 15:50:29 -0400

    post merge

--- analysis.py
+++ analysis.py
@@ -2,11 +2,11 @@ import chess
 import chess.engine
 
 def sf_calc(fen, check):
-    # stockfish = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/stockfish/15.1/bin/stockfish")
-    # leela = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/lc0/0.29.0/bin/lc0")
+    stockfish = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/stockfish/15.1/bin/stockfish")
+    leela = chess.engine.SimpleEngine.popen_uci("/usr/local/Cellar/lc0/0.29.0/bin/lc0")
     
-    stockfish = chess.engine.SimpleEngine.popen_uci("/home/jfernandes33/packages/stockfish/15.1/bin/stockfish")
-    leela = chess.engine.SimpleEngine.popen_uci("/home/jfernandes33/packages/lc0/0.29.0/bin/lc0")
+    # stockfish = chess.engine.SimpleEngine.popen_uci("/home/jfernandes33/packages/stockfish/15.1/bin/stockfish")
+    # leela = chess.engine.SimpleEngine.popen_uci("/home/jfernandes33/packages/lc0/0.29.0/bin/lc0")
     board = chess.Board()
     board.set_fen(fen)
     response = stockfish.analyse(board, chess.engine.Limit(time=.1))
